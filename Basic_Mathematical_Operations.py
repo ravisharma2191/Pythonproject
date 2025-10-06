303846cf6704d0860a49d403cd97f0a2d5b52fa1
ravisharma2191 <ravisharma.bmchrc@gail.com>
2025-10-06 20:08:17 +0530

    file upload

---
+++ Basic_Mathematical_Operations.py
@@ -0,0 +1,12 @@
+#
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+# operations
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+print(f"Addition: {int(c)}")
+print(f"Subtraction: {int(d)}")
+print(f"Multiplication: {int(e)}")
+print(f"Division: {int(f) if f.is_integer() else f}")
---
+++ Personalized_Greeting.py
@@ -0,0 +1,6 @@
+#
+fname = input(f"Enter your first name: ")
+mname= input(f"Enter your middle name: ")
+lname= input(f"Enter your last name: ")
+#
+print(f"Hello,{fname} {mname} {lname}! ,"" Welcome to the python program.")
