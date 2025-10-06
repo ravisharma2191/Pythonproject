91ff13021b72d38dfb86bed2ba8b02c5a056b318
ravisharma2191 <ravisharma.bmchrc@gail.com>
2025-10-06 20:02:23 +0530

    Inital file

--- Basic_Mathematical_Operations.py
+++ Basic_Mathematical_Operations.py
@@ -1,1 +1,12 @@
-My name is ravi kumar sharma
+#
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+# Performing operations
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+print(f"Addition: {int(c)}")
+print(f"Subtraction: {int(d)}")
+print(f"Multiplication: {int(e)}")
+print(f"Division: {int(f) if f.is_integer() else f}")
